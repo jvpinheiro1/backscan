24538e4987355a612564d5ba2bf5e931f8e9e5d2
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-25 15:21:08 -0300

    teste

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7348162841:AAHxPqMkCjxLMS29Kbz9hNVYp8RMDQDDbxs"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "1494831240"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude } = req.body;
